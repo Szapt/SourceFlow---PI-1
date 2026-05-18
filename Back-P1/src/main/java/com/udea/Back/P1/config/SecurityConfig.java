72d91012a7f08dd5992d5eac76b64f8d761ac9c4
MariAgudelo2 <maria.aagudelo@udea.edu.co>
2026-05-18 13:08:24 -0500

    fix: modify CORS configuration to allow all localhost domains

--- Back-P1/src/main/java/com/udea/Back/P1/config/SecurityConfig.java
+++ Back-P1/src/main/java/com/udea/Back/P1/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "https://connected-nacho-sleet.ngrok-free.dev"));
+        config.setAllowedOriginPatterns(List.of("http://localhost:*", "https://connected-nacho-sleet.ngrok-free.dev"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
