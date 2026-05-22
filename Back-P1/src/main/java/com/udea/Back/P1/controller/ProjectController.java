02b8c52e1c2bcd28f41497081a5733e3798c44fa
MariAgudelo2 <maria.aagudelo@udea.edu.co>
2026-05-21 22:49:06 -0500

    refactor: trying to see if Santiago's file change

--- Back-P1/src/main/java/com/udea/Back/P1/controller/ProjectController.java
+++ Back-P1/src/main/java/com/udea/Back/P1/controller/ProjectController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+// probando probando 
 @RestController
 @RequestMapping("/projects")
 public class ProjectController {
